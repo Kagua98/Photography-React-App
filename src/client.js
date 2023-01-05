7b2842664cefe3caa780561dcf66018a453930cf
Kagua98 <markkagua@gmail.com>
2023-01-05 09:02:37 +0300

    Secure Sanity Credentials

--- src/client.js
+++ src/client.js
@@ -1,10 +1,11 @@
 import sanityClient from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
+//Import credentials from Sanity ('npm run dev')
 export const client = sanityClient({
-    projectId: 'g5srqz19',
-    dataset: 'production',
-    apiVersion: '202-02-01',
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    dataset: process.env.REACT_APP_SANITY_DATASET,
+    apiVersion: process.env.REACT_APP_SANITY_API_VERSION,
     useCdn: true,
-    token: 'skMiEu2GuHm434Ei49cizWhlRWZMfGFs2gniifaXkKNj1aasRKtzZ4duhNphUialqTWQ7xHGfS8s74r3IejMag1O6aPLhhHl7WHw7QOICpbS2gYrU3Wk1cJSIK5oF2gOBqRsdWXQkwCIs3l2kbeGaoG5v6zi6rfTNtq4Q6Yb2SVwCN2SkqJY',
+    token: process.env.REACT_APP_SANITY_TOKEN,
 });
