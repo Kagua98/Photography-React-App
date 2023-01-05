588ebe3448c322e871f8074a3b358e6d0053252c
Kagua98 <markkagua@gmail.com>
2023-01-05 09:07:44 +0300

    Add Sanity credentials

---
+++ src/client.js
@@ -0,0 +1,11 @@
+import sanityClient from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+
+//Import credentials from Sanity ('npm run dev')
+export const client = sanityClient({
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    dataset: process.env.REACT_APP_SANITY_DATASET,
+    apiVersion: process.env.REACT_APP_SANITY_API_VERSION,
+    useCdn: true,
+    token: process.env.REACT_APP_SANITY_TOKEN,
+});
